495531e1da7074afba38facb6599a1ce03ce1b21
suriyadi <suryadi.surya5@gmail.com>
2021-03-12 22:44:21 +0700

    init

---
+++ src/main/java/com/example/main.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class main {
+    public static void main(String[] args) {
+
+        /*String[] randomNames = {"nia", "budi", "anto"};
+
+        for (int i = 0; i < randomNames.length; i++){
+            System.out.println(randomNames[i]);
+        }
+
+        for(String i : randomNames){
+            System.out.println(i);
+        }*/
+
+        /*ArrayList<String> randomNames = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            randomNames.add("name_" + i);
+        }
+
+        randomNames.forEach(x -> System.out.println(x));*/
+
+        /*HashMap<String, Integer> numberMaps = new HashMap<>();
+        numberMaps.put("one", 1);
+        numberMaps.put("two", 2);
+        numberMaps.put("three", 3);
+        numberMaps.put("four", 4);
+        numberMaps.put("five", 5);
+
+        System.out.println("number three: " + numberMaps.get("three").toString());
+
+        for (String key: numberMaps.keySet()){
+            System.out.println(key + " : " + numberMaps.get(key).toString());
+        }*/
+
+        System.out.println("Hello World");
+    }
+}
